3394df940ae2eb242efceedf578eca2fa62e82e0
epeng333 <bethpeng86@yahoo.com>
2025-07-10 16:22:09 -0700

    Add section and photo for SCRL2 lesson

---
+++ src/sections/TestSection.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Scroller from "../lib/Scroller.svelte";
+    import ArticleText from "../lib/ArticleText.svelte";
+</script>
+
+<div>
+    <Scroller layout="left">
+        {#snippet sticky()}
+            <img class="plane-img" src="plane.jpg" alt="plane!" />           
+        {/snippet}
+    
+        {#snippet scrolly()}
+            <ArticleText>
+                Even if it feels unfamiliar...
+            </ArticleText>
+
+            <ArticleText>
+                Give it a try.
+            </ArticleText>
+
+            <ArticleText>
+                The possibilities are endless.
+            </ArticleText>
+        {/snippet}
+    </Scroller> 
+</div>
